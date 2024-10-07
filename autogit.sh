d218588b7a71b32255b30d55a40137e89aa227c5
theaux <theaux@archlinux.com>
2024-10-07 23:58:30 +0200

    :technologist: Improved autogit.sh

--- autogit.sh
+++ autogit.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# Clean build files
-make fclean
+clear
 
-# Ask for commit message
+make fclean > /dev/null
+
+norm_errors=$(norminette | grep 'Error:' | wc -l)
+
+yellow='\033[38;2;234;179;8m'
+reset='\e[0m'
+bold='\033[1m'
+green='\033[38;2;0;255;0m'
+
+if [ "$norm_errors" -ne 0 ]; then
+    printf "[$yellow WARNING $reset]: Il y a$yellow $norm_errors$reset erreurs de norminette.\n"
+    read -p "Voulez-vous afficher les erreurs? (Y/n): " show_errors
+    show_errors=${show_errors:-Y}
+    if [[ $show_errors =~ ^[Yy]$ ]]; then
+        norminette | grep 'Error'
+    fi
+else
+    printf "[$green INFO $reset]: Aucune erreur de norminette.\n"
+fi
+
 read -p "Entrez le message de commit: " commit_message
-
-# Confirm the commit message
 echo "Message de commit: '$commit_message'"
 read -p "Confirmez-vous ce message de commit? (Y/n): " confirmation
 
-# Default to 'Y' if no input is given
 confirmation=${confirmation:-Y}
 
 if [[ $confirmation =~ ^[Yy]$ ]]; then
