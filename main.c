7ac1d2c927c1c7b8f72d2411b143e424f4d22f7f
theaux <theaux@archlinux.com>
2024-10-10 19:22:36 +0200

    🔥 Removed Deprecated functions

--- main.c
+++ main.c
@@ -6,45 +6,18 @@
 /*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 20:14:00 by tbabou            #+#    #+#             */
-/*   Updated: 2024/10/07 23:37:53 by tbabou           ###   ########.fr       */
+/*   Updated: 2024/10/10 19:21:25 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token_type	get_redirection_type(char *str);
-
 int	ft_exit(char *line)
 {
-	free(line);
+	char	**args;
+
+	args = ft_split(line, ' ');
 	printf("exit\n");
-	return (0);
-}
-
-int	is_option(char *str)
-{
-	if (str[0] == '-')
-		return (1);
-	return (0);
-}
-
-int	ft_chrinstr(char *str, char c)
-{
-	while (str && *str)
-	{
-		if (*str == c)
-			return (1);
-		str++;
-	}
-	return (0);
-}
-
-int	is_string(char *str)
-{
-	if (ft_chrinstr(str, '"'))
-		return (1);
-	if (ft_chrinstr(str, '\''))
-		return (2);
 	return (0);
 }
 
@@ -55,7 +28,8 @@ int	main(void)
 
 	while (1)
 	{
-		line = readline(PROMPT);
+		printf("%s%s%s ", VIOLET500, PROMPT, RESET);
+		line = readline("→ ");
 		if (line)
 		{
 			if (ft_strncmp(line, "exit", 4) == 0)
