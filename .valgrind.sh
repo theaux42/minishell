2484747c0d1a92f4a56eebcc53683042fa0ce15d
theaux <theaux@archlinux.com>
2024-10-12 00:00:35 +0200

    :building_construction: Delete of old file structure

---
+++ .valgrind.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all --suppressions="other/valgrind-rules" ./minishell
--- valgrind.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-make
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all --suppressions="valgrind-rules" ./minishell
