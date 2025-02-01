cd1c8fca99aabfca8845e0abc98184d98f156979
theaux42 <tbabou@student.42.fr>
2025-02-01 11:56:19 +0000

    :fire: removed -c use

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 20:14:00 by tbabou            #+#    #+#             */
-/*   Updated: 2025/02/01 00:21:02 by tbabou           ###   ########.fr       */
+/*   Updated: 2025/02/01 11:55:58 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,45 +63,19 @@ void	main_loop(t_minishell *minishell)
 	}
 }
 
-int	execute_single_line(t_minishell *minishell, char *cmd_line)
-{
-    int	status;
-
-    minishell->line = ft_strdup(cmd_line);
-    if (!minishell->line)
-        return (ft_dprintf(2, ERR_MALLOC), 1);
-    if (*minishell->line)
-    {
-        minishell->commands = get_commands(minishell->line, minishell);
-        if (minishell->commands)
-            execute_commands(minishell);
-        minishell->commands = NULL;
-    }
-    status = minishell->status;
-	exit_parent(NULL, minishell, false);
-    return (status);
-}
-
 int	main(int ac, char **av, char **env)
 {
-    t_minishell	*minishell;
+	t_minishell	*minishell;
 
-    if (ac > 1 && ft_strncmp(av[1], "-c", 3) == 0)
-    {
-        if (ac != 3)
-            return (ft_dprintf(2, "Usage: %s -c \"command\"\n", av[0]), 1);
-        minishell = init_minishell(env);
-        if (!minishell)
-            return (1);
-        return (execute_single_line(minishell, av[2]));
-    }
-    if (!isatty(STDIN_FILENO))
-        return (ft_dprintf(2, ERR_NOT_A_TTY), 1);
-    if (DEBUG_MODE)
-        ft_dprintf(2, DEBUG_MSG);
-    minishell = init_minishell(env);
-    if (!minishell)
-        return (1);
-    main_loop(minishell);
-    return (0);
+	(void)ac;
+	(void)av;
+	if (!isatty(STDIN_FILENO))
+		return (ft_dprintf(2, ERR_NOT_A_TTY), 1);
+	if (DEBUG_MODE)
+		ft_dprintf(2, DEBUG_MSG);
+	minishell = init_minishell(env);
+	if (!minishell)
+		return (1);
+	main_loop(minishell);
+	return (0);
 }
